c7b4b87f1d391c197fd38aa0f3dca27dcf027034
syl0702 <navyblue794@gmail.com>
2023-08-24 11:50:20 +0900

    Create 1차

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 
@@ -20,3 +21,24 @@ def detail(request, id):
     }
     
     return render(request, 'detail.html', context)
+
+
+
+def create(request):
+    # 사용자가 입력한 데이터를 DB에 저장
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', id=article.id)
+
+    # 사용자가 데이터를 입력할 수 있도록 빈 종이를 리턴
+    else:
+        form = ArticleForm()
+
+        context = {
+            'form': form,
+        }
+
+        return render(request, 'create.html', context)
